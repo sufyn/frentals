6bf4abf6378a7c3eb3e18ff96c46ce9d952b564c
sufyn <suyaan4guys@gmail.com>
2023-06-01 18:43:27 +0530

    new

---
+++ forms/get_posts.php
@@ -0,0 +1,34 @@
+<?php
+// Connect to the MySQL database
+require('server.php');
+// Retrieve the city parameter from the query string
+$city = $_GET['city'];
+
+// Prepare and execute the SQL query to retrieve posts based on the city
+$stmt = $con->prepare('SELECT * FROM posts WHERE location = ?');
+$stmt->bind_param('s', $city);
+$stmt->execute();
+$result = $stmt->get_result();
+if ($result){
+$images = [];
+foreach ($result as $results) {
+  $images[] = [
+    'city' => $results['location'],
+    'image' => 'posts/' . $results['data'],
+    'content' => $results['content'],
+    'date' => $results['create_datetime']
+  ];
+}
+}
+
+else{
+    echo "Error: " . $query . "<br>" . mysqli_error($con);
+}
+// Close the database connection
+$stmt->close();
+$con->close();
+
+// Return the posts as JSON
+header('Content-Type: application/json');
+echo json_encode($images);
+?>
